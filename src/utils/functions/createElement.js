d7fc66f1e9dabd5d82b8fbdb9ec3d10c34f04b25
ShinEunJin <sineun5501@naver.com>
2023-06-15 16:05:00 +0900

    createElement 유틸 함수 추가

---
+++ src/utils/functions/createElement.js
@@ -0,0 +1,25 @@
+export const createElement = (tag, attributes = {}, ...children) => {
+  if (!tag || typeof tag !== "string") {
+    throw Error("tag is required and type must be string.");
+  }
+
+  const element = document.createElement(tag);
+
+  if (typeof attributes !== "object" || Array.isArray(attributes)) {
+    throw Error("attributes must be object only.");
+  }
+
+  if (attributes) {
+    for (const [key, value] of Object.entries(attributes)) {
+      element.setAttribute(key, value);
+    }
+  }
+
+  if (children && children.length > 0) {
+    for (const child of children) {
+      element.append(child);
+    }
+  }
+
+  return element;
+};
