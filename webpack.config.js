0bc15e8872951001485f9e967292be1621aafcf6
ShinEunJin <sineun5501@naver.com>
2023-07-05 21:51:15 +0900

    webpack typescript 골뱅이 별칭 사용용

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,9 @@ module.exports = {
     ],
   },
   resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
     extensions: [".ts", ".js"],
   },
   devtool: "inline-source-map",
